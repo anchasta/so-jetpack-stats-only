aa77be45f08f693084903e975c697ad00171f07b
Piet Bos <webmaster@senlinonline.com>
2014-01-13 16:46:40 +0800

    change text domain

--- modules/module-info.php
+++ modules/module-info.php
@@ -16,7 +16,7 @@ function stats_more_info() { ?>
 	</div>
 
 	<h4><?php esc_html_e( 'Jetpack Stats Module', 'so-jetpack-stats-only' ); ?></h4>
-	<p><?php esc_html_e( 'There are many plugins and services that provide statistics, but data can be overwhelming. WordPress.com Stats makes the most popular metrics easy to understand through a clear and attractive interface.', 'jetpack' ) ?></p>
+	<p><?php esc_html_e( 'There are many plugins and services that provide statistics, but data can be overwhelming. WordPress.com Stats makes the most popular metrics easy to understand through a clear and attractive interface.', 'so-jetpack-stats-only' ) ?></p>
 <?php
 }
 add_action( 'jetpack_module_more_info_stats', 'stats_more_info' );
@@ -29,17 +29,17 @@ function stats_more_info_connected() { ?>
 	</div>
 
 	<h4><?php esc_html_e( 'Jetpack Stats Module', 'so-jetpack-stats-only' ); ?></h4>
-	<p><?php esc_html_e( 'There are many plugins and services that provide statistics, but data can be overwhelming. WordPress.com Stats makes the most popular metrics easy to understand through a clear and attractive interface.', 'jetpack' ) ?></p>
+	<p><?php esc_html_e( 'There are many plugins and services that provide statistics, but data can be overwhelming. WordPress.com Stats makes the most popular metrics easy to understand through a clear and attractive interface.', 'so-jetpack-stats-only' ) ?></p>
 	<?php
 	// Only show link to stats dashboard when stats has been activated already.
 	//if ( ( defined( 'IS_WPCOM' ) ) && function_exists( 'stats_get_csv' ) ) {
 	if ( function_exists( 'stats_admin_bar_head' ) ) {
-		printf( '<p>' . __( 'You can <a href="%s">view your stats dashboard here</a>.', 'jetpack' ) . '</p>', admin_url( 'admin.php?page=stats' ) );
+		printf( '<p>' . __( 'You can <a href="%s">view your stats dashboard here</a>.', 'so-jetpack-stats-only' ) . '</p>', admin_url( 'admin.php?page=stats' ) );
 	}	
 }
 add_action( 'jetpack_module_more_info_connected_stats', 'stats_more_info_connected' );
 
 function stats_load_more_link( $description ) {
-	echo '<a class="button-secondary more-info-link" href="http://en.support.wordpress.com/stats/">' . esc_html__( 'Learn More', 'jetpack' ) . '</a>';
+	echo '<a class="button-secondary more-info-link" href="http://en.support.wordpress.com/stats/">' . esc_html__( 'Learn More', 'so-jetpack-stats-only' ) . '</a>';
 }
 add_filter( 'jetpack_learn_more_button_stats', 'stats_load_more_link' );
